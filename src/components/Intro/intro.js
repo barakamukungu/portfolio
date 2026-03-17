78662e0354f4deb57c7109a3d2d50dbf369efb8f
barakamukungu <bethelybaraka@gmail.com>
2026-03-17 11:06:02 +0300

    Update intro.js

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -10,8 +10,9 @@ const Intro = () => {
       <div className='introContent'>
         <span  className='hello'></span>
         <span  className='introText'>I'm <span className='introName'>Baraka Mukungu</span> <br />Data Analyst | SQL • Python • Excel | Cybersecurity & Log Analysis</span>
-        <p className='introPara'>I specialize in analyzing large datasets to uncover insights, build dashboards, and support decision-making. <br />My work includes customer sentiment analysis, sales performance dashboards, and security log analysis.</p>
-        <Link><button className='btn'><img src={btnImg} alt='Hire Me' className='btnImg' /> Hire Me</button></Link>
+        <p className='introPara'>I analyze large datasets to uncover insights, build dashboards, and support data-driven decision-making.  <br /> 
+          My projects include customer sentiment analysis, sales performance dashboards, and security log analysis. </p>
+        <Link><button className='btn'><img src={btnImg} alt='View My Work' className='btnImg' /> View My Work</button></Link>
       </div>
       <img src={bg} alt='Profile' className='bg' />
     </section>
