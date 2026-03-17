27b27f11c185ed5c21901793cbcf4ca01b68e31c
barakamukungu <bethelybaraka@gmail.com>
2026-03-17 11:12:59 +0300

    Update works.js

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -9,15 +9,24 @@ const Works = () => {
   return (
     <section id='works'>
       <h2 className='worksTitle'>My Projects</h2>
-      <span className='worksDesc'>I take pride in paying attention to the smallest details and ensuring the best output. Click on the images to view.</span>
-      <div className='worksImgs'>
-        <a href='https://food-delivery-home.onrender.com'><img src={Portfolio6} alt='' className='worksImg' /></a>
-        <a href='https://blog-app-dlvr.onrender.com'><img src={Portfolio2} alt='' className='worksImg' /></a>
-        <a href='https://github.com/barakamukungu/simscanner'><img src={Portfolio3} alt='' className='worksImg' /></a>
-        <a href='https://github.com/barakamukungu/log-analyzer'><img src={Portfolio4} alt='' className='worksImg' /></a>
-        {/*<img src={Portfolio5} alt='' className='worksImg' />
-        <img src={Portfolio1} alt='' className='worksImg' />*/}
-      </div>
+      <span className='worksDesc'>I take pride in paying attention to the smallest details and ensuring the best output. Click on the images to view my work.</span>
+     <div className='worksImgs'>
+      <a href='https://github.com/barakamukungu/amazon_analysis'>
+        <img src={Portfolio2} alt='Amazon Analysis' className='worksImg' />
+      </a>
+    
+      <a href='https://github.com/barakamukungu/crm-sales-dashboard'>
+        <img src={Portfolio3} alt='CRM Dashboard' className='worksImg' />
+      </a>
+    
+      <a href='https://github.com/barakamukungu/coffee-shop-analysis'>
+        <img src={Portfolio4} alt='Coffee Analysis' className='worksImg' />
+      </a>
+    
+      <a href='https://github.com/barakamukungu/security-log-analysis'>
+        <img src={Portfolio6} alt='Security Analysis' className='worksImg' />
+      </a>
+    </div>
       {/*<button className='worksBtn'>See More</button>*/}
     </section>
   );
